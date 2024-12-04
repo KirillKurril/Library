163e0e86e2685e07ca767996ca68bb2c62f94b16
KirillKurril <kirillkurill1525@gmail.com>
2024-12-04 15:43:22 +0300

    Presenter started. Need to replase records fields with dto's

--- Library.Domain/Entities/Book.cs
+++ Library.Domain/Entities/Book.cs
@@ -10,10 +10,11 @@ namespace Library.Domain.Entities
         public Genre Genre { get; set; }
         public int AuthorId { get; set; }
         public string? ImageUrl { get; set; }
-        public bool IsAvailable { get; set; }
+        public bool IsAvailable => BorrowedAt is null;
         public int UserId { get; set; }
         public DateTime? BorrowedAt { get; set; }
         public DateTime? ReturnDate { get; set; }
         public DateTime? ActualReturnDate { get; set; }
     }
 }
+//Background tasks with hosted services in ASP.NET Core
---
+++ Library.Domain/Entities/UserBooks.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain.Entities
+{
+    public class UserBooks
+    {
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+        public DateOnly BorrowDate { get; set; }
+        public DateOnly ReturnDate { get; set; }
+    }
+}
